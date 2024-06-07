63b4df786886b844f2ba00def28e85fe2b482652
kwasinsiah <truegrayowl@gmail.com>
2024-06-07 08:45:04 -0400

    created detail panel component

---
+++ src/app/ui/detail-panel/detail-panel.component.ts
@@ -0,0 +1,101 @@
+import {Component, Input} from '@angular/core';
+import {Task} from "../../../models/task";
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+import {SubTask} from "../../../models/subTask";
+import {TaskService} from "../../services/task.service";
+import {InputTextModule} from "primeng/inputtext";
+import {Button} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+
+@Component({
+  selector: 'app-detail-panel',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgForOf,
+    NgIf,
+    InputTextModule,
+    Button,
+    DropdownModule
+  ],
+  templateUrl: './detail-panel.component.html',
+  styleUrl: './detail-panel.component.scss'
+})
+export class DetailPanelComponent {
+  @Input() task: Task = {};
+
+  editTask = {
+    newSubtask: '',
+    title: this.task.title,
+    status: this.task.status,
+    startDate: this.task.startDate,
+    dueDate: this.task.dueDate,
+    priority: this.task.priority
+  }
+  priorities:Task['priority'][] = [
+    'Critical', 'High', 'Medium', 'Low', 'Trivial'
+  ]
+  statuses: Task['status'][] = [
+    'Completed', 'In-progress', 'Todo'
+  ]
+
+
+
+  newSubtaskMode = false;
+  editMode = false;
+
+  constructor(private taskService: TaskService) {
+  }
+
+  completeSubtask(subtask: SubTask) {
+    console.log('subtask completed')
+    subtask.status = 'Completed'
+    this.task.subtasks = this.task.subtasks?.map(oldSubtask => {
+      if (oldSubtask.title === subtask.title) {
+        return subtask;
+      }
+      return oldSubtask
+    })
+
+    this.taskService.updateTask(this.task)
+  }
+
+  addSubtask() {
+    console.log('add subtask clicked.');
+    const subtask: SubTask = {
+      title: this.editTask.newSubtask,
+      status: 'Todo'
+    }
+    this.editTask.newSubtask = '';
+    this.taskService.addSubtask(this.task, subtask);
+    this.toggleNewSubtaskInputs();
+  }
+  toggleNewSubtaskInputs() {
+    this.newSubtaskMode = !this.newSubtaskMode;
+  }
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+  }
+  updateTask() {
+    this.toggleEditMode();
+  }
+  updatePriority() {
+    console.log('updated priority')
+    if (this.task) {
+      this.task.priority = this.editTask.priority;
+      this.taskService.updateTask(this.task)
+    } else {
+      console.log('no task selected')
+    }
+  }
+  updateStatus() {
+    console.log('updated status')
+    if (this.task) {
+      this.task.status = this.editTask.status;
+      this.taskService.updateTask(this.task)
+    } else {
+      console.log('no task selected')
+    }
+  }
+}
